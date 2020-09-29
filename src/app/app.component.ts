2bc72ce2e21cb828b3e44bd2d5dd831925ad6c01
Evgeniy Kvach <evgeniy.kvach@newdv.ru>
2020-09-29 17:24:12 +0300

    beginning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+
 })
-export class AppComponent {
-  title = 'angular-basics';
+export class AppComponent implements OnInit{
+  posts: Post[] = [
+    {title: 'хочу выучить Ангуляр', text: 'Все еще хочу выучить', id: 1},
+    {title: 'Следующий блок', text: 'про директивы и пайпы', id: 2}
+  ];
+
+  updatePosts = (post) => {
+    this.posts = [post, ...this.posts];
+  }
+  deletePost = (id: number) => {
+    console.log('id');
+    this.posts = this.posts.filter(p => p.id !== id);
+  }
+
+  ngOnInit(): void {
+  }
 }
