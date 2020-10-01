e2ec3f80d3d063c4ae857aceeef0ab15ba829269
Evgeniy Kvach <evgeniy.kvach@newdv.ru>
2020-10-01 15:24:11 +0300

    pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import {Component} from '@angular/core';
+import {Observable} from 'rxjs';
 
+export interface Post {
+  title: string;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -8,5 +13,38 @@ import {Component} from '@angular/core';
 
 })
 export class AppComponent{
-  isVisible = true;
+  e: number = Math.E;
+  str = 'hello world';
+  date: Date = new Date();
+  float = 0.42;
+  obj = {
+    a: 1,
+    b: {
+      c: 2,
+      d: {
+        e: 3,
+        f: 4
+      }
+    }
+  };
+  search = '';
+  searchField = 'title';
+
+  posts: Post[] = [
+    {title: 'Beer', text: 'Самое лучшее пиво в мире'},
+    {title: 'Bread', text: 'The best bread in the world'},
+    {title: 'Javascript', text: 'The best language in the world'},
+  ];
+
+  p: Promise<string> = new Promise<string>(resolve => {
+    setTimeout(() => {
+      resolve('Promise resolved');
+    }, 4000);
+  });
+  addPost = () => {
+    this.posts.unshift({
+      title: 'Angular 9',
+      text: 'Text'
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
+import localeRu from '@angular/common/locales/ru';
+
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import {StyleDirective} from './directives/style.directive';
 import { IfnotDirective } from './directives/ifnot.directive';
+import {registerLocaleData} from '@angular/common';
+import { MultByPipe } from './pipes/mult-by.pipe';
+import { ExMarksPipe } from './pipes/ex-marks.pipe';
+import { FilterPipe } from './pipes/filter.pipe';
+import { AsyncPipePipe } from './pipes/async-pipe.pipe';
+
+registerLocaleData(localeRu);
 
 @NgModule({
   declarations: [
     AppComponent,
     StyleDirective,
     IfnotDirective,
+    MultByPipe,
+    ExMarksPipe,
+    FilterPipe,
+    AsyncPipePipe,
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'ru' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
