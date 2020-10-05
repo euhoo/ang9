f76359b9ec098b873c8918a069f841c891472c76
Evgeniy Kvach <evgeniy.kvach@newdv.ru>
2020-10-05 13:14:19 +0300

    forms update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {LocalCounterService} from './services/local-counter.service';
-import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
-import {MyValidators} from './my.validators';
 export interface Post {
   title: string;
   text: string;
@@ -13,47 +11,9 @@ export interface Post {
   styleUrls: ['./app.component.scss'],
   providers: [LocalCounterService]
 })
-export class AppComponent implements  OnInit{
-  form: FormGroup;
-  ngOnInit(): void {
-    this.form = new FormGroup({
-      email: new FormControl('', [
-        Validators.email,
-        Validators.required,
-        MyValidators.restrictedEmail,
-      ], [
-        MyValidators.uniqEmail
-      ]),
-      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
-      address: new FormGroup({
-        country: new FormControl('by'),
-        city: new FormControl('Минск', Validators.required)
-      }),
-      skills: new FormArray([])
-    });
-  }
-  setCapital = () => {
-    const cityMap = {
-      ru: 'Москва',
-      ua: 'Киев',
-      by: 'Минск'
-    };
-    const country = this.form.get('address').get('country').value;
-    const city = cityMap[country];
-    this.form.patchValue({address: {city}});
-  }
-  addSkill = () => {
-    const control = new FormControl('', Validators.required);
-    (this.form.get('skills') as FormArray).push(control);
-  }
-
-  submit(): void {
-    if (this.form.valid) {
-      //
-    }
-    console.log('form:', this.form);
-    const formData = {...this.form.value};
-    console.log('formdata:', formData);
-    this.form.reset();
+export class AppComponent{
+  appState = 'on';
+  handleChange = () => {
+    console.log(this.appState);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import localeRu from '@angular/common/locales/ru';
 import { AppComponent } from './app.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {registerLocaleData} from '@angular/common';
+import { SwitchComponent } from './switch/switch.component';
 
 registerLocaleData(localeRu);
 
 @NgModule({
   declarations: [
     AppComponent,
+    SwitchComponent,
   ],
   imports: [
     BrowserModule,
